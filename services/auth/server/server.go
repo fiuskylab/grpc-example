d245ffb19e476bcecd2dddf703adf0ccaff20fdd
Rafael <rafael_breno@outlook.com>
2021-10-29 22:01:04 -0300

    feat: Implement server

---
+++ services/auth/server/server.go
@@ -0,0 +1,36 @@
+package server
+
+import (
+	"context"
+
+	"github.com/fiuskylab/grpc-example/common"
+	"github.com/fiuskylab/grpc-example/proto"
+	"github.com/fiuskylab/grpc-example/services/auth/repository"
+)
+
+type Server struct {
+	proto.UnimplementedAuthServiceServer
+	common *common.Common
+	Repo   *repository.AuthRepoCtx
+}
+
+func NewServer(c *common.Common, r *repository.AuthRepoCtx) (*Server, error) {
+	s := Server{
+		common: c,
+		Repo:   r,
+	}
+
+	return &s, nil
+}
+
+func (s *Server) Create(ctx context.Context, req *proto.CreateUserRequest) (*proto.CreateUserResponse, error) {
+	return s.Repo.Create(req), nil
+}
+
+func (s *Server) Login(ctx context.Context, req *proto.LoginRequest) (*proto.LoginResponse, error) {
+	return s.Repo.Login(req), nil
+}
+
+func (s *Server) Check(ctx context.Context, req *proto.CheckTokenRequest) (*proto.CheckTokenResponse, error) {
+	return s.Repo.Check(req), nil
+}
