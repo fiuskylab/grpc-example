f00b86f98254388c159770788b0222ba124a2872
Rafael <rafael_breno@outlook.com>
2021-10-29 21:52:38 -0300

    fix: Entity user to suppor UUID

--- services/auth/entity/user.go
+++ services/auth/entity/user.go
@@ -3,13 +3,14 @@ package entity
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID        uint   `gorm:"primaryKey;default:gen_random_uuid()"`
-	Username  string `db:"username" json:"username" gorm:"varchar(128);unique;not null"`
-	Password  string `db:"password" json:"password" gorm:"varchar(128);not null"`
+	ID        uuid.UUID `gorm:"primaryKey;default:gen_random_uuid()"`
+	Username  string    `db:"username" json:"username" gorm:"varchar(128);unique;not null"`
+	Password  string    `db:"password" json:"password" gorm:"varchar(128);not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
